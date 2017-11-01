82881db48a44fcf47e62ea38f85ee5fc650a464c
Gleb Kalachev <megas781@gmail.com>
2017-11-01 15:49:43 +0300

    alpha 1.01
    1) корректировка интерфейса комментария
    2) добавлен resignAnyFirstResponder
    3) PostViewController рабоатет корректно

--- fiveCardsTechnicalTask/PostViewController.swift
+++ fiveCardsTechnicalTask/PostViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import SwiftyJSON
 class PostViewController: UIViewController {
    
    
@@ -17,7 +17,35 @@ class PostViewController: UIViewController {
    var nValue: Int!
    
    override func viewWillAppear(_ animated: Bool) {
-      print("post nValue: \(nValue)")
+      guard let n = nValue else {
+         print("Не смогу извлечь nValue в PostViewController'е")
+         return
+      }
+      
+      var url = URL.init(string: "https://jsonplaceholder.typicode.com/posts/")!
+      url.appendPathComponent("\(n)")
+      print("url: \(url)")
+      let session = URLSession.shared.dataTask(with: url) { (data, response, error) in
+         if let error = error {
+            print("error: \(error.localizedDescription)")
+         }
+         
+         guard let data = data else {
+            print("couldn't retrieve data")
+            return
+         }
+         
+         let json = JSON.init(data: data)
+         
+         DispatchQueue.main.async {
+            
+            self.titleLabel.text = json["title"].string ?? "[couldn't retrieve title]"
+            self.bodyTextView.text = json["body"].string ?? "[couldn't retrieve body]"
+         }
+      }
+      
+      session.resume()
+      
    }
    
 }
