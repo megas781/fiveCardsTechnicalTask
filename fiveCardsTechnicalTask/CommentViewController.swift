c8ae6483f3e474fdf77d915a9814318aa28c3b2c
Gleb Kalachev <megas781@gmail.com>
2017-11-01 14:55:47 +0300

    установил outlet'ы

---
+++ fiveCardsTechnicalTask/CommentViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CommentViewController.swift
+//  fiveCardsTechnicalTask
+//
+//  Created by Gleb Kalachev on 11/1/17.
+//  Copyright © 2017 Gleb Kalachev. All rights reserved.
+//
+
+import UIKit
+
+class CommentViewController: UIViewController {
+   
+   @IBOutlet weak var nameLabel: UILabel!
+   @IBOutlet weak var emailLabel: UILabel!
+   @IBOutlet weak var bodyTextView: UITextView!
+   
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      
+      // Do any additional setup after loading the view.
+   }
+   
+}
---
+++ fiveCardsTechnicalTask/PostViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PostViewController.swift
+//  fiveCardsTechnicalTask
+//
+//  Created by Gleb Kalachev on 11/1/17.
+//  Copyright © 2017 Gleb Kalachev. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+   
+   
+   @IBOutlet weak var titleLabel: UILabel!
+   
+   @IBOutlet weak var bodyTextView: UITextView!
+   
+}
