9e83d5d30377d89b1de08e0964a7fd4ebc49963e
Gleb Kalachev <megas781@gmail.com>
2017-11-01 13:58:28 +0300

    С интерфейсом разобрались

--- fiveCardsTechnicalTask/ViewController.swift
+++ fiveCardsTechnicalTask/ViewController.swift
@@ -11,35 +11,46 @@ import SwiftyJSON
 class ViewController: UIViewController {
 
    
+   @IBOutlet weak var postsView: CardView!
    
    
    
    override func viewDidLoad() {
       super.viewDidLoad()
       
-      let session = URLSession.shared.dataTask(with: URL.init(string: "https://jsonplaceholder.typicode.com/posts/1")!) { (data, response, error) in
-         guard error == nil else {
-//            print("error: \((error as! NSError).localizedDescription)")
-            return
-         }
-         
-         if let response = response {
-//            print("response: \(response)")
-         }
-         
-         guard let data = data else {
-            print("empty data")
-            return
-         }
-         
-         let json = JSON.init(data: data)
-         
-         print("title: \(json["title"])")
-         
-         
-      }
       
-      session.resume()
+      
+      
+      
+      
+      
+      
+      
+      
+      
+//      let session = URLSession.shared.dataTask(with: URL.init(string: "https://jsonplaceholder.typicode.com/posts/1")!) { (data, response, error) in
+//         guard error == nil else {
+////            print("error: \((error as! NSError).localizedDescription)")
+//            return
+//         }
+//         
+//         if let response = response {
+////            print("response: \(response)")
+//         }
+//         
+//         guard let data = data else {
+//            print("empty data")
+//            return
+//         }
+//         
+//         let json = JSON.init(data: data)
+//         
+//         print("title: \(json["title"])")
+//         
+//         
+//      }
+//      
+//      session.resume()
       
       
       
